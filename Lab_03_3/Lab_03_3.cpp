860953581889733347718d6315a1ffce218fb787
mINFLEE <maxkotovych@gmail.com>
2024-10-16 17:22:47 +0300

    first commit

---
+++ Lab_03_3/Lab_03_3.cpp
@@ -0,0 +1,40 @@
+// Lab_03_3.cpp
+// Котович Максим Михайлович
+// Лабораторна робота № 3.3
+// Розгалуження, задане графіком функції.
+// Варіант 20
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double x;
+    double R; 
+    double y; 
+
+    cout << "R = "; cin >> R;
+    cout << "x = "; cin >> x;
+
+    if (x <= -2)
+        y = 1;
+    else 
+        if (-2 < x && x <= 0) 
+            y = 1; 
+        else 
+            if (0 < x && x <= R)
+                y = -sqrt(R * R - x * x);
+            else 
+                if (R < x && x <= 2 * R)
+                    y = sqrt(R * R - (x - R) * (x - R));
+                else
+                    y = -(x - 2 * R) / (6 - 2 * R) - 1;
+
+    cout << endl;
+    cout << "y = " << y << endl;
+
+    cin.get();
+    return 0;
+}
