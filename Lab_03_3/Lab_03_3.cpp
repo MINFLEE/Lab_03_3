78947d306f960f4a78e6cdfdc5228388b8e6c02d
mINFLEE <maxkotovych@gmail.com>
2024-10-27 16:36:09 +0300

    second commit

--- Lab_03_3/Lab_03_3.cpp
+++ Lab_03_3/Lab_03_3.cpp
@@ -12,25 +12,22 @@ using namespace std;
 int main()
 {
     double x;
-    double R; 
-    double y; 
+    double R;
+    double y;
 
     cout << "R = "; cin >> R;
     cout << "x = "; cin >> x;
 
-    if (x <= -2)
+    if (x <= 0)
         y = 1;
-    else 
-        if (-2 < x && x <= 0) 
-            y = 1; 
-        else 
-            if (0 < x && x <= R)
-                y = -sqrt(R * R - x * x);
-            else 
-                if (R < x && x <= 2 * R)
-                    y = sqrt(R * R - (x - R) * (x - R));
-                else
-                    y = -(x - 2 * R) / (6 - 2 * R) - 1;
+    else
+        if (0 < x && x <= R)
+            y = -sqrt(R * R - x * x);
+        else
+            if (R < x && x <= 2 * R)
+                y = sqrt(R * R - (x - R) * (x - R));
+            else
+                y = -(x - 2 * R) / (6 - 2 * R) - 1;
 
     cout << endl;
     cout << "y = " << y << endl;
